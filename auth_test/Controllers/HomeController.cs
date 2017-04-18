59c44a9b7efbce1e54b7d00a31dd0e86457321cb
인상학 <platinasnow@naver.com>
2017-04-18 11:40:30 +0900

    초기 커밋.

---
+++ auth_test/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using auth_test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace auth_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles =  "Admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Authorize(Roles = "Editor")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, String returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            User user = new User() { Email = model.Email, Password = model.Password };
+            user = Repository.GetUserDetails(user);
+
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(model.Email, false);
+
+                var authTicket = new FormsAuthenticationTicket(1, user.Email, DateTime.Now, DateTime.Now.AddMinutes(20), false, user.Roles);
+                string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+                var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                HttpContext.Response.Cookies.Add(authCookie);
+                return RedirectToAction("Index", "Home");
+            }
+
+            else
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            //AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ auth_test/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace auth_test.Models
+{
+    public class User 
+    {
+        public string Email { get; set; }
+        public string Roles { get; set; }
+        public string Password { get; set; }
+
+    }
+
+    public static class Repository
+    {
+        static List<User> users = new List<User>() {
+
+        new User() {Email="admin",Roles="Admin,Editor",Password="1" },
+        new User() {Email="editor",Roles="Editor",Password="1" }
+    };
+
+        public static User GetUserDetails(User user)
+        {
+            return users.Where(u => u.Email.ToLower() == user.Email.ToLower() &&
+            u.Password == user.Password).FirstOrDefault();
+        }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "전자 메일")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "암호")]
+        public string Password { get; set; }
+
+        [Display(Name = "사용자 이름 및 암호 저장")]
+        public bool RememberMe { get; set; }
+    }
+}
